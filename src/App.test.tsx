0bf4fe8b83a1ec7110fb7c77a597a947039185db
강수호(sooohka) <shkorea1004@gmail.com>
2022-04-14 10:58:15 +0900

    chore: add eslint configuration
    - eslint airbnb룰 추가
    - prettier 추가
    - settings.json 파일 추가
    - 불필요한 파일들 삭제

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("a", () => {
+  it("renders learn react link", () => {
+    expect.assertions(1);
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
